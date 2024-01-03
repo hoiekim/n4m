315711d31a6696ccd87c0d22773c65d4e41f2daa
hoiekim <github@hoie.kim>
2024-01-02 20:26:10 -0800

    adds distribution main entrypoint

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export * from "./adaptor/adaptor";
+export * from "./analyzer/analyzer";
+export * from "./lib/array";
+export * from "./lib/chords";
+export * from "./pitch_shifter/pitch_shifter";
+export * from "./stepper/stepper";
